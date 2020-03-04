bf82ccec74d3b6b1eeb79d1980ec55a0919ee09b
flint <olusegun.sola19@gmail.com>
2020-03-04 10:52:04 +0100

    moves routes files to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { Fragment }from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/dashboard';
+import Home from './components/home';
+import Navbar from './components/navigation/Routes';
 import './App.css';
-import Routes from './components/navigation/Routes';
+
 
 
 function App() {
   return (
-      <div className="App">
-        <Routes />
-      </div>
+    <Fragment>
+    <Router>
+        <Navbar />
+        <Switch>
+            <div>
+                
+                <Route exact path='/' component={Home} />
+                <Route path='/dashboard/mymeetings'  component={Dashboard} />
+                <Route path='/dashboard/meetingrooms'  component={Dashboard} />
+                <Route path='/dashboard/createmeeting' component={Dashboard} />
+            </div>
+        </Switch>
+    </Router>
+
+</Fragment>
     
   );
 }
