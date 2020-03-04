e96b785601ac25288a1274df4bd94ef24cece7e3
flint <olusegun.sola19@gmail.com>
2020-03-04 10:07:34 +0100

    changed exact component to component

--- src/components/navigation/Routes.js
+++ src/components/navigation/Routes.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Dashboard from '../dashboard/Dashboard';
 import Home from '../home/Home';
 import Navbar from '../home/Navbar';
@@ -13,10 +13,10 @@ const Routes = () => {
                 <Switch>
                     <div>
                         
-                        <Route exact path= '/' component={Home} />
-                        <Route path= '/dashboard/mymeetings'  component={Dashboard} />
-                        <Route path= '/dashboard/meetingrooms' component={Dashboard} />
-                        <Route path= '/dashboard/createmeeting'  component={Dashboard} />
+                        <Route exact path='/' component={Home} />
+                        <Route path='/dashboard/mymeetings'  component={Dashboard} />
+                        <Route path='/dashboard/meetingrooms'  component={Dashboard} />
+                        <Route path='/dashboard/createmeeting' component={Dashboard} />
                     </div>
                 </Switch>
             </Router>
